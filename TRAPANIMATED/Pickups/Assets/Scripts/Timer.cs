c23c6592619f67eba4246317071257373d658828
Lana <suhossii@gmail.com>
2019-01-12 18:54:13 +0000

    Timer

---
+++ TRAPANIMATED/Pickups/Assets/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public int timeLeft = 60;
+    public Text countdownText;
+    public GameObject panel;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownText.text = ("Time Left = " + timeLeft);
+
+        if (timeLeft <= 0)
+        {
+            StopCoroutine("LoseTime");
+            countdownText.text = "Times Up!";
+            panel.SetActive(true);
+
+        }
+    }
+
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
