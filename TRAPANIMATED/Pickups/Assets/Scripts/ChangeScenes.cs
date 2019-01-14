92e9fbc370190352885e6176f34a03b348290351
Mayionn <43456657+Mayionn@users.noreply.github.com>
2019-01-14 23:57:53 +0000

    Font changes, added buttons, updated main menu sprites

---
+++ TRAPANIMATED/Pickups/Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour {
+
+    public void continueGameLevel1()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void tryAgainLevel1()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void tryAgainLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void backMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
